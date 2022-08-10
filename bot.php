3c7d38ca8d54b0ebcf4e39fd75385a8bd95a25ef
Cristian Curtinaz <curtinazif@gmail.com>
2022-08-10 13:34:05 -0300

    Accept only integer

--- bot.php
+++ bot.php
@@ -31,7 +31,9 @@ $discord->on('ready', function (Discord $discord) {
         // Dados de todos os tamanhos
         if (strrpos($message->content, '$roll d') === 0) {
             $tamanho = explode('$roll d', $message->content);
-            $message->reply(rand(1, $tamanho[1]));
+            if((int)$tamanho) {
+                $message->reply(rand(1, (int)$tamanho[1]));
+            }
         }
 
         if ($message->content == '$d20') {
